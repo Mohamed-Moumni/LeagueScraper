63a42b1f88339c7dad82dd4d62e674493179bf48
mohamed.moumni <mohamed.moumni@umniabank.ma>
2025-12-29 16:27:29 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import asyncio
+import zendriver as zd
+
+PATH = "https://www.sofascore.com/football/match/olympic-safi-wydad-casablanca/"
+
+
+async def main():
+    pass
+
+if __name__ == "__main__":
+    asyncio.run(main())
