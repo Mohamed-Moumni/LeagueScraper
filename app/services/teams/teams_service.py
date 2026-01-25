c9a045f50dc3048ea46aea34c0acdd475099ce6c
Mohamed-Moumni <mmoumni@student.1337.ma>
2026-01-25 14:54:20 +0100

    feat(scraper): add league table endpoint with full standings data

--- app/services/teams/teams_service.py
+++ app/services/teams/teams_service.py
@@ -20,3 +20,15 @@ class TeamsScraper(Scraper):
         except Exception as e:
             logger.error(f"Error fetching teams: {e}")
             return None
+    
+    async def get_team_table(self, league_id:int, season_id:int):
+        URL:str = self.base_url + f"/api/v1/tournament/{league_id}/season/{season_id}/standings/total"
+        try:
+            teams_data = await self.get_api_response(URL)
+            if teams_data is None:
+                return None
+            standings = teams_data["standings"][0]
+            return standings["rows"]
+        except Exception as e:
+            logger.error(f"Error fetching teams: {e}")
+            return None
